3027e40969233a2a67d1bfde54ce37b52d2943eb
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-01-24 15:18:32 -0500

    board work

--- board.rb
+++ board.rb
@@ -1,10 +1,11 @@
-require_relative "afternoon_test.rb"
+require_relative "card.rb"
 
 class Board
 
     def initialize(size)
         @grid = Array.new(size){ Array.new(size, "_") }
         @size = size
+        @pairs_num = (@size ** 2 ) / 2
     end
 
     def [](pos)
@@ -17,14 +18,11 @@ class Board
         @grid[row][col] = val
     end
 
-
     def grid_random
         array = []
-        2.times {array << (0...@size).sample}
+        2.times {array << rand(0...@size)}
         array
     end
-
-    
 
     def valid?(pos)
         row, col = pos
@@ -34,15 +32,50 @@ class Board
     end
 
     def empty?(pos)
-        @grid[pos] == "_"
+        row, col = pos
+        self[pos] == "_"
     end
 
     def populate
-        @pairs_num = (@size ** 2 ) / 2
-        pos = self.grid_random
-        if valid?(pos) && empty?(pos)
-            @grid[pos] =  #card gets put here
+        counter = 0
+
+        until counter == @pairs_num
+            pair = []
+            until pair.length == 2
+                pos = grid_random
+                pair << pos if valid?(pos) && empty?(pos)
+            end
+            temp = Card.new
+            pair.each { |pos| self[pos] = temp }
+            counter += 1
+        end
     end
 
-
 end
+
+
+
+
+
+
+
+
+       
+#         if valid?(pos) && empty?(pos)
+#             @grid[pos] =  #card gets put here
+#     end
+
+#     Outside loop - stop when board is full
+#     Inner loop - when we have a pair of locations
+
+
+# We're going to intiialize a grid
+# While that grid.lenth is less than 2, 
+# we're going to get a random position, check if tis valid and empty, thens hovel it into the array
+# Then when taht array is two, we're going to set our grid at those locations, equal to a new card
+
+# # Count how many pairs we have placed so far
+# # When that count is equal to num pairs, we're done
+# Until that's the case, then we have another until, we need to get a position, 
+
+
